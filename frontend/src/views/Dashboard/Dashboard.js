9fd6c8d40e6ca09392b86ae799b6f634feed2f3b
Abidijihed <abidij55@gmail.com>
2022-09-16 09:22:53 +0100

    almoste done for frontend work v 0.0.1

--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -21,10 +21,22 @@ import {
 	Text,
 	Th,
 	Thead,
-	Tr
+	Tr,
+	GridItem 
 } from '@chakra-ui/react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 // Styles for the circular progressbar
 import medusa from 'assets/img/cardimgfree.png';
+import myreact from 'assets/img/myreact.png'
+import mynode from 'assets/img/mynode.jpg'
+import myexpress from 'assets/img/myexpress.jpg'
+import mymysql from 'assets/img/mymysql.jpg'
+import mynext from 'assets/img/mynext.png'
+import myvue from 'assets/img/myvue.png'
+import myangular from 'assets/img/myangular.png'
+import mymaterial from 'assets/img/mymaterial.jpg'
 // Custom components
 import Card from 'components/Card/Card.js';
 import CardBody from 'components/Card/CardBody.js';
@@ -54,7 +66,7 @@ import { Carousel } from 'react-bootstrap';
 
 export default function Dashboard() {
 	return (
-		<Flex flexDirection='row' pt={{ base: '120px', md: '75px' }}  >
+		<Flex flexDirection='column' pt={{ base: '120px', md: '75px' }}  >
 	
 
 	<Grid
@@ -63,119 +75,248 @@ export default function Dashboard() {
 				gap='24px'
 				mb='24px'>
 				{/* Sales Overview */}
-				<div id="earth"></div>
 
 				{/* Active Users */}
 				<Card p='16px'>
-					<CardBody>
-						<Flex direction='column' w='100%'>
-							<Box
-								bg='linear-gradient(126.97deg, #060C29 28.26%, rgba(4, 12, 48, 0.5) 91.2%)'
-								borderRadius='20px'
-								display={{ sm: 'flex', md: 'block' }}
-								justify={{ sm: 'center', md: 'flex-start' }}
-								align={{ sm: 'center', md: 'flex-start' }}
-								minH={{ sm: '180px', md: '220px' }}
-								p={{ sm: '0px', md: '22px' }}>
-								<BarChart
-									barChartOptions={barChartOptionsDashboard}
-									barChartData={barChartDataDashboard}
-								/>
-							</Box>
-							<Flex direction='column' mt='24px' mb='36px' alignSelf='flex-start'>
-								<Text fontSize='lg' color='#fff' fontWeight='bold' mb='6px'>
-									Active Users
-								</Text>
-								<Text fontSize='md' fontWeight='medium' color='gray.400'>
-									<Text as='span' color='green.400' fontWeight='bold'>
-										(+23%)
-									</Text>{' '}
-									than last week
-								</Text>
-							</Flex>
-							<SimpleGrid gap={{ sm: '12px' }} columns={4}>
-								<Flex direction='column'>
-									<Flex alignItems='center'>
-										<IconBox as='box' h={'30px'} w={'30px'} bg='brand.200' me='6px'>
-											<WalletIcon h={'15px'} w={'15px'} color='#fff' />
-										</IconBox>
-										<Text fontSize='sm' color='gray.400'>
-											Users
-										</Text>
-									</Flex>
-									<Text
-										fontSize={{ sm: 'md', lg: 'lg' }}
-										color='#fff'
-										fontWeight='bold'
-										mb='6px'
-										my='6px'>
-										32,984
-									</Text>
-									<Progress colorScheme='brand' bg='#2D2E5F' borderRadius='30px' h='5px' value={20} />
-								</Flex>
-								<Flex direction='column'>
-									<Flex alignItems='center'>
-										<IconBox as='box' h={'30px'} w={'30px'} bg='brand.200' me='6px'>
-											<RocketIcon h={'15px'} w={'15px'} color='#fff' />
-										</IconBox>
-										<Text fontSize='sm' color='gray.400'>
-											Clicks
-										</Text>
-									</Flex>
-									<Text
-										fontSize={{ sm: 'md', lg: 'lg' }}
-										color='#fff'
-										fontWeight='bold'
-										mb='6px'
-										my='6px'>
-										2.42m
-									</Text>
-									<Progress colorScheme='brand' bg='#2D2E5F' borderRadius='30px' h='5px' value={90} />
-								</Flex>
-								<Flex direction='column'>
-									<Flex alignItems='center'>
-										<IconBox as='box' h={'30px'} w={'30px'} bg='brand.200' me='6px'>
-											<CartIcon h={'15px'} w={'15px'} color='#fff' />
-										</IconBox>
-										<Text fontSize='sm' color='gray.400'>
-											Sales
-										</Text>
-									</Flex>
-									<Text
-										fontSize={{ sm: 'md', lg: 'lg' }}
-										color='#fff'
-										fontWeight='bold'
-										mb='6px'
-										my='6px'>
-										2,400$
-									</Text>
-									<Progress colorScheme='brand' bg='#2D2E5F' borderRadius='30px' h='5px' value={30} />
-								</Flex>
-								<Flex direction='column'>
-									<Flex alignItems='center'>
-										<IconBox as='box' h={'30px'} w={'30px'} bg='brand.200' me='6px'>
-											<StatsIcon h={'15px'} w={'15px'} color='#fff' />
-										</IconBox>
-										<Text fontSize='sm' color='gray.400'>
-											Items
-										</Text>
-									</Flex>
-									<Text
-										fontSize={{ sm: 'md', lg: 'lg' }}
-										color='#fff'
-										fontWeight='bold'
-										mb='6px'
-										my='6px'>
-										320
-									</Text>
-									<Progress colorScheme='brand' bg='#2D2E5F' borderRadius='30px' h='5px' value={50} />
-								</Flex>
-							</SimpleGrid>
-						</Flex>
-					</CardBody>
+				<div class="container1">
+    <img className='myimage' src="https://yetiacademy.com/wp-content/uploads/2020/09/5-Fantastic-Coding-Games-For-Students-scaled.jpeg" alt="Lake louise picture" class="image1"/>
+    <img className='myimage' src="https://9b16f79ca967fd0708d1-2713572fef44aa49ec323e813b06d2d9.ssl.cf2.rackcdn.com/1140x_a10-7_cTC/college0620-1532138146.jpg" alt=""/>
+    <img className='myimage' src="https://cf.ltkcdn.net/teens/images/std/175601-425x282r1-comp-sci.jpg" alt=""/>
+    <img  className='myimage' src="https://cdn7.dissolve.com/p/D943_130_039/D943_130_039_1200.jpg" alt=""/>
+    <img  className='myimage' src="https://st.focusedcollection.com/16485780/i/650/focused_250562908-stock-photo-teacher-helping-junior-high-students.jpg" alt=""/>
+ 
+                 </div>
 				</Card>
+			
+					<div id="earth"></div>
+			
+	
+	
 			</Grid>
-			</Flex>
+			<Grid templateColumns={{ sm: '1fr', md: '1fr 1fr', '2xl': '2fr 1.2fr 1.5fr' }} my='26px' gap='18px'>
+				{/* Welcome Card */}
+				<Card
+					p='10px'
+					gridArea={{ md: '1 / 1 / 2 / 3', '2xl': 'auto' }}
+					// bgImage={medusa}
+					bgSize='cover'
+					bgPosition='50%'>
+							<Text color='#fff' fontSize='lg' fontWeight='bold' mb='10px'>
+							To reach your dream set your goals and follow a good Méthodologie
+</Text>
+<br></br>
+		<Row >
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		We teach JavaScript, but we want to stay language-agnostic
+</Text>
+<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg' />
+
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+This means that no matter what language you learn, you will have the skills to be able to learn it.<br></br>
+        This means that we want you to learn how to learn
+</Text>
+
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		Difference between university & immersive
+</Text>
+<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+Learn more skills that realy you need them 
+Working in team (pairs)<br></br>
+More practical experience
+</Text>
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		Before you ask a lecturer a question please try the following:
+		</Text>
+		<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+Discuss it with your partner<br></br>
+Look in the documentation<br></br>
+Look at answers on Google and Stack Overflow<br></br>
+Ask your neighbor
+
+</Text>
+
+        </Col>
+		<Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		We want you to learn how to teach yourself 
+		</Text>
+		<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+
+		<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+		because one day, Pavel, Charlie and Gisela will not be there to answer your questions
+</Text>
+		        </Col>
+      </Row>
+				</Card>
+				{/* Satisfaction Rate */}
+				<Card gridArea={{ md: '2 / 1 / 3 / 2', '2xl': 'auto' }}>
+					<CardHeader mb='24px'>
+						<Flex direction='column'>
+							<Text color='#fff' fontSize='lg' fontWeight='bold' mb='4px'>
+							Technical Skills & Technology
+							</Text>
+							
+						</Flex>
+					</CardHeader>
+					
+					<Row >
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+React Js
+</Text>
+		<img id='mytechnology' src={myreact}/>
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+Vue Js
+</Text>
+		<img id='mytechnology' src={myvue}/>
 		
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+Angular Js
+</Text>
+		<img id='mytechnology' src={myangular}/>
+		
+        </Col>
+		<Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		Material Ui
+		</Text>
+		<img id='mytechnology' src={mymaterial}/>
+			
+		
+		        </Col>
+      </Row>
+	  <Row >
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='2px' textDecoration='underline'>
+Node Js
+</Text>
+		<img id='mytechnology' src={mynode}/>
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+Express Js
+</Text>
+		<img id='mytechnology' src={myexpress}/>
+		
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+Next Js
+</Text>
+		<img id='mytechnology' src={mynext}/>
+		
+        </Col>
+		<Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+My-SQL
+</Text>
+		<img id='mytechnology' src={mymysql}/>
+		
+		        </Col>
+      </Row>
+				
+				</Card>
+				{/* Referral Tracking */}
+				<Card gridArea={{ md: '2 / 2 / 3 / 3', '2xl': 'auto' }}>
+					<Flex direction='column'>
+						<Flex justify='space-between' align='center' mb='40px'>
+							<Text color='#fff' fontSize='lg' fontWeight='bold' fontFamily="serif" >
+                            Why NOJA Code ?
+							</Text>
+							
+						</Flex>
+						<Flex direction={{ sm: 'column', md: 'row' }}>
+						<Text color='#fff' fontSize='18px' fontFamily="cursive" mb='4px'>
+						Our coding bootcamps are challenging, life-changing, and designed to fit your schedule and skill level. We train students using a computer science and coding curriculum that models the exciting work being done in the software engineering industry. We'll help you launch the career you crave.
+						<br></br>
+						<br></br>
+Led by passionate and experienced instructors, engineers, and career advisors, our graduates become software engineers that companies around the world want to hire.
+</Text>
+					</Flex>
+					</Flex>
+				</Card>
+				
+			</Grid>
+			<Grid templateColumns={{ sm: '1fr', md: '1fr 1fr', '2xl': '2fr 1.2fr 1.5fr' }} my='26px' gap='18px'>
+				{/* Welcome Card */}
+				<Card
+					p='10px'
+					gridArea={{ md: '1 / 1 / 2 / 3', '2xl': 'auto' }}
+					// bgImage={medusa}
+					bgSize='cover'
+					bgPosition='50%'>
+							<Text color='#fff' fontSize='lg' fontWeight='bold' mb='10px' textAlign='center' fontFamily="serif">
+							            Our Coding Bootcamps Schedule
+							</Text>
+<br></br>
+		<Row >
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		We teach JavaScript, but we want to stay language-agnostic
+</Text>
+<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg' />
+
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+This means that no matter what language you learn, you will have the skills to be able to learn it.<br></br>
+        This means that we want you to learn how to learn
+</Text>
+
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		Difference between university & immersive
+</Text>
+<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+Learn more skills that realy you need them 
+Working in team (pairs)<br></br>
+More practical experience
+</Text>
+        </Col>
+        <Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		Before you ask a lecturer a question please try the following:
+		</Text>
+		<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+
+<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+Discuss it with your partner<br></br>
+Look in the documentation<br></br>
+Look at answers on Google and Stack Overflow<br></br>
+Ask your neighbor
+
+</Text>
+
+        </Col>
+		<Col xs={6} md={3}>
+		<Text color='#fff' fontSize='17px' fontFamily="serif" mb='4px' textDecoration='underline'>
+		We want you to learn how to teach yourself 
+		</Text>
+		<img id='goodthink' src='https://www.hubspot.com/hubfs/how-to-learn-coding.jpg'/>
+
+		<Text color='#fff' fontSize='14px' fontFamily="cursive" mb='4px'>
+		because one day, Pavel, Charlie and Gisela will not be there to answer your questions
+</Text>
+		        </Col>
+      </Row>
+				</Card>
+			
+				
+			</Grid>
+
+			</Flex>
+
 	);
 }
