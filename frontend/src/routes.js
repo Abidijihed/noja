61e48ed0e4280156715ec876340d8b42197596e8
Abidijihed <abidij55@gmail.com>
2022-09-23 22:23:01 +0100

    done with the first part of routers v 0.0.1

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -18,75 +18,147 @@ import AccountBoxIcon from '@mui/icons-material/AccountBox';
 import InfoIcon from '@mui/icons-material/Info';
 import AppRegistrationIcon from '@mui/icons-material/AppRegistration';
 
-
+var dashRoutes
+const onveriveconnection=sessionStorage.getItem('_stud')
+ if(onveriveconnection){
+  dashRoutes = [
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      rtlName: "لوحة القيادة",
+      icon: <HomeIcon color='inherit' />,
+      component: Dashboard,
+      layout: "/auth",
+    },
+    {
+      path: "/Contact",
+      name: "Contact",
+      rtlName: "لوحة القيادة",
+      icon: <EmailIcon color='inherit' />,
+      component: Contact,
+      layout: "/auth",
+    },
+    {
+      path: "/About",
+      name: "About",
+      rtlName: "لوحة القيادة",
+      icon: <InfoIcon color='inherit' />,
+      component: About,
+      layout: "/auth",
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      rtlName: "لوحة القيادة",
+      icon: <AccountBoxIcon color='inherit' />,
+      secondaryNavbar: true,
+      component: Profile,
+      layout: "/auth",
+    },
+    {
+      path: "/Sudents",
+      name: "Sudents",
+      rtlName: "لوحة القيادة",
+      icon: <StatsIcon color='inherit' />,
+      component: Sudents,
+      layout: "/auth",
+    },
+    {
+      path: "/JoinUs",
+      name: "Join Us",
+      rtlName: "لوحة القيادة",
+      icon: <AppRegistrationIcon color='inherit' />,
+      component: JoinUs,
+      layout: "/auth",
+    },
+    {
+      
+      category: "account",
+      rtlName: "صفحات",
+      state: "pageCollapse",
+      views: [
+       
+        {
+          path: "/SignIn/redirectUrl=https%3A%2F%2Flearn.noja.co%2F",
+          name: "SignIn",
+          rtlName: "لوحة القيادة",
+          icon: <AppRegistrationIcon color='inherit' />,
+          component: Dashboard,
+          layout: "/auth",
+        },
+      ],
+    },
+  ];
+ }else{
+  dashRoutes = [
+    {
+      path: "/dashboard",
+      name: "Dashboard",
+      rtlName: "لوحة القيادة",
+      icon: <HomeIcon color='inherit' />,
+      component: Dashboard,
+      layout: "/auth",
+    },
+    {
+      path: "/Contact",
+      name: "Contact",
+      rtlName: "لوحة القيادة",
+      icon: <EmailIcon color='inherit' />,
+      component: Contact,
+      layout: "/auth",
+    },
+    {
+      path: "/About",
+      name: "About",
+      rtlName: "لوحة القيادة",
+      icon: <InfoIcon color='inherit' />,
+      component: About,
+      layout: "/auth",
+    },
+    {
+      path: "/profile",
+      name: "Profile",
+      rtlName: "لوحة القيادة",
+      icon: <AccountBoxIcon color='inherit' />,
+      secondaryNavbar: true,
+      component: SignIn,
+      layout: "/auth",
+    },
+    {
+      path: "/Sudents",
+      name: "Sudents",
+      rtlName: "لوحة القيادة",
+      icon: <StatsIcon color='inherit' />,
+      component: SignIn,
+      layout: "/auth",
+    },
+    {
+      path: "/JoinUs",
+      name: "Join Us",
+      rtlName: "لوحة القيادة",
+      icon: <AppRegistrationIcon color='inherit' />,
+      component: JoinUs,
+      layout: "/auth",
+    },
+    {
+      
+      category: "account",
+      rtlName: "صفحات",
+      state: "pageCollapse",
+      views: [
+       
+        {
+          path: "/SignIn/redirectUrl=https%3A%2F%2Flearn.noja.co%2F",
+          name: "SignIn",
+          rtlName: "لوحة القيادة",
+          icon: <AppRegistrationIcon color='inherit' />,
+          component: SignIn,
+          layout: "/auth",
+        },
+      ],
+    },
+  ];
+ }
  
- var dashRoutes = [
-  {
-    path: "/dashboard",
-    name: "Dashboard",
-    rtlName: "لوحة القيادة",
-    icon: <HomeIcon color='inherit' />,
-    component: Dashboard,
-    layout: "/auth",
-  },
-  {
-    path: "/Contact",
-    name: "Contact",
-    rtlName: "لوحة القيادة",
-    icon: <EmailIcon color='inherit' />,
-    component: Contact,
-    layout: "/auth",
-  },
-  {
-    path: "/About",
-    name: "About",
-    rtlName: "لوحة القيادة",
-    icon: <InfoIcon color='inherit' />,
-    component: About,
-    layout: "/auth",
-  },
-  {
-    path: "/profile",
-    name: "Profile",
-    rtlName: "لوحة القيادة",
-    icon: <AccountBoxIcon color='inherit' />,
-    secondaryNavbar: true,
-    component: Profile,
-    layout: "/auth",
-  },
-  {
-    path: "/Sudents",
-    name: "Sudents",
-    rtlName: "لوحة القيادة",
-    icon: <StatsIcon color='inherit' />,
-    component: Sudents,
-    layout: "/auth",
-  },
-  {
-    path: "/JoinUs",
-    name: "Join Us",
-    rtlName: "لوحة القيادة",
-    icon: <AppRegistrationIcon color='inherit' />,
-    component: JoinUs,
-    layout: "/auth",
-  },
-  {
-    
-    category: "account",
-    rtlName: "صفحات",
-    state: "pageCollapse",
-    views: [
-     
-      {
-        path: "/SignIn/redirectUrl=https%3A%2F%2Flearn.noja.co%2F",
-        name: "SignIn",
-        rtlName: "لوحة القيادة",
-        icon: <AppRegistrationIcon color='inherit' />,
-        component: SignIn,
-        layout: "/auth",
-      },
-    ],
-  },
-];
 // }
 export default dashRoutes;
