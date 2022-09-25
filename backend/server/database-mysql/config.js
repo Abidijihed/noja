f6cb033507b863e167209069e4eed543c86e94f5
Abidijihed <abidij55@gmail.com>
2022-09-25 15:45:08 +0100

    save

--- backend/server/database-mysql/config.js
+++ backend/server/database-mysql/config.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql2');
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '127.0.0.1',
   user: 'root',
   password: 'Ji31826832',
   database: 'noja',
