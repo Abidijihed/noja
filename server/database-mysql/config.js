e3a5c9c21d60287437993ee22f8a479ad3801546
Abidijihed <abidij55@gmail.com>
2022-10-14 15:00:35 +0100

    save

--- server/database-mysql/config.js
+++ server/database-mysql/config.js
@@ -2,10 +2,9 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
   host: 'db-mysql-fra1-44899-do-user-11624328-0.b.db.ondigitalocean.com',
   user: 'jihed',
-  port:'25060',
+  port:25060,
   password: 'AVNS_blS-FZtYk_U9Le2zKtr',
   database: 'noja',
-  insecureAuth : true
 });
 connection.connect((err) => {
   if (err) throw err;
