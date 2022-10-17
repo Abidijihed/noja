18888e0f2d4f6af34bd545594184b81ce3ee4088
Abidijihed <abidij55@gmail.com>
2022-10-17 12:51:15 +0100

    save

--- frontend/src/views/Dashboard/Dashboard.js
+++ frontend/src/views/Dashboard/Dashboard.js
@@ -57,7 +57,6 @@ export default function Dashboard() {
 	  
 	return (
 		<Flex flexDirection="column" pt={{ base: '125px', md: '175px' }}>
-		{console.log(response,'youyou')}
 			<Grid
 				templateColumns={{ sm: '1fr', lg: '1.7fr 1.3fr' }}
 				maxW={{ sm: '100%', md: '100%' }}
